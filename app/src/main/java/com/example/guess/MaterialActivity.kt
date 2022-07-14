8f54e0e107ec5ce1b02f171425fb8d5818135b75
Ally <sanatus4521@gmail.com>
2022-07-14 17:41:33 +0800

    Material design

---
+++ app/src/main/java/com/example/guess/MaterialActivity.kt
@@ -0,0 +1,65 @@
+package com.example.guess
+
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.appcompat.app.AlertDialog
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.guess.databinding.ActivityMaterialBinding
+import kotlinx.android.synthetic.main.content_material.*
+
+class MaterialActivity : AppCompatActivity() {
+    val secretNumber = SecretNumber()
+    val TAG = MainActivity::class.java.simpleName
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMaterialBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+     binding = ActivityMaterialBinding.inflate(layoutInflater)
+     setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        val navController = findNavController(R.id.nav_host_fragment_content_material)
+        appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+
+        binding.fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+    val navController = findNavController(R.id.nav_host_fragment_content_material)
+    return navController.navigateUp(appBarConfiguration)
+            || super.onSupportNavigateUp()
+    }
+
+    fun check(view: View) {
+        val n = ed_number.text.toString().toInt()
+        Log.d(TAG, "number: " + n)
+        val validate = secretNumber.validate(n)
+        val message:String
+        if (validate > 0) {
+            message = getString(R.string.bigger)
+        } else if (validate < 0) {
+            message = getString(R.string.lower)
+        } else {
+            message = getString(R.string.yes_secret_number_is) + secretNumber.secret
+        }
+//        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+        AlertDialog.Builder(this)
+            .setTitle(getString(R.string.Dialog_title))
+            .setMessage(message).setPositiveButton(getString(R.string.ok), null)
+            .show()
+    }
+}

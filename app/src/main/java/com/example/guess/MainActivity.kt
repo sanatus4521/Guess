f2b02e76ed3c9394d875c132b5c189b7707f8595
Ally <sanatus4521@gmail.com>
2022-07-28 17:41:09 +0800

    Recycler (inner class版本)

--- app/src/main/java/com/example/guess/MainActivity.kt
+++ app/src/main/java/com/example/guess/MainActivity.kt
@@ -3,37 +3,57 @@ package com.example.guess
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.row_function.view.*
 
 class MainActivity : AppCompatActivity() {
-    val secretNumber = SecretNumber()
     val TAG = MainActivity::class.java.simpleName
+    val function = listOf<String>(
+        "Camera",
+        "Guess game",
+        "Record list",
+        "Download coupons",
+        "News",
+        "Maps"
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        Log.d(TAG, "Secret number is: " + secretNumber.secret)
+
+        // Recycler View
+        recycler.layoutManager = LinearLayoutManager(this)
+        recycler.setHasFixedSize(true)
+        recycler.adapter = FunctionAdapter()
+    }
+    inner class FunctionAdapter() : RecyclerView.Adapter<FuntionHolder>() {
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FuntionHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.row_function, parent, false)
+            val holder = FuntionHolder(view)
+            return holder
+        }
+
+        override fun onBindViewHolder(holder: FuntionHolder, position: Int) {
+            holder.nameText.text = function.get(position)
+        }
+
+        override fun getItemCount(): Int {
+            return function.size
+        }
+
     }
 
-    fun check(view: View) {
-        val n = ed_number.text.toString().toInt()
-        Log.d(TAG, "number: " + n)
-        val validate = secretNumber.validate(n)
-        val message:String
-        if (validate > 0) {
-            message = getString(R.string.bigger)
-        } else if (validate < 0) {
-            message = getString(R.string.lower)
-        } else {
-            message = getString(R.string.yes_you_got_it) + secretNumber.secret
-        }
-//        Toast.makeText(this, message, Toast.LENGTH_LONG).show()
-        AlertDialog.Builder(this)
-            .setTitle(getString(R.string.Dialog_title))
-            .setMessage(message).setPositiveButton(getString(R.string.ok), null)
-            .show()
+    class FuntionHolder(view: View) : RecyclerView.ViewHolder(view) {
+        var nameText: TextView = view.name
     }
+
 }

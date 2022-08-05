adfbee8ee36c5a17c76c29d998c9b381793e34ee
Ally <sanatus4521@gmail.com>
2022-08-05 18:38:40 +0800

    spinner

--- app/src/main/java/com/example/guess/MainActivity.kt
+++ app/src/main/java/com/example/guess/MainActivity.kt
@@ -3,9 +3,12 @@ package com.example.guess
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -26,12 +29,27 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val TAG = MainActivity::class.java.simpleName
 
         // Recycler View
         recycler_record_list.layoutManager = LinearLayoutManager(this)
         recycler_record_list.setHasFixedSize(true)
         recycler_record_list.adapter = FunctionAdapter()
 
+        //spinner
+        val spinnerData = arrayOf("Blue", "Gray", "pink", "purple")
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,
+        spinnerData)
+        adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line)
+        spinner.adapter = adapter
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+                Log.d(TAG, "OnItemSelectedListener: ${spinnerData[position]}")
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+        }
     }
     inner class FunctionAdapter() : RecyclerView.Adapter<FuntionHolder>() {
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FuntionHolder {
